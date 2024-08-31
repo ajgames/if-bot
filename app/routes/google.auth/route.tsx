8b12eca3f67b84180c4f4f10ffe6d2b510021739
Adam <adam@xyzdigital.com>
2024-08-31 12:03:01 -0700

    Add more permissions

--- app/routes/google.auth/route.tsx
+++ app/routes/google.auth/route.tsx
@@ -3,7 +3,10 @@ import { googleOauth2Client } from "~/utils/googleOauthClient";
 
 export const loader: LoaderFunction = () => {
   const scopes = [
-    "https://www.googleapis.com/auth/gmail.readonly",
+    'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.metadata',
+  'https://www.googleapis.com/auth/gmail.labels',
     'https://www.googleapis.com/auth/pubsub'
   ];
 
